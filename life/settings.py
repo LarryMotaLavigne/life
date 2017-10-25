9c8d0f8d85b6dbbe3c47012a83cbaf249aa95447
squalex <larry.motalavigne@gmail.com>
2017-10-25 23:33:35 +0200

    fix Heroku db config

--- life/settings.py
+++ life/settings.py
@@ -138,13 +138,19 @@ if TRAVIS_ENVIRONMENT:
         }
     }
 elif HEROKU_ENVIRONMENT:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'localbdd',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
