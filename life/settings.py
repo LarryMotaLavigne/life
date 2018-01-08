b397d620ae81ab2f1d63701c1e58aab7543c9e96
squalex <larry.motalavigne@gmail.com>
2018-01-08 16:21:03 +0100

    Admin interface + Heroku database config

--- life/settings.py
+++ life/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'life.context_processors.from_settings',  # Admin site color and environment warning
             ],
         },
     },
@@ -103,14 +104,24 @@ if os.getenv('BUILD_ON_TRAVIS', None):
         }
     }
 elif 'HEROKU' in os.environ:
+    ENVIRONMENT_NAME = 'PRODUCTION'
+    ENVIRONMENT_COLOR = 'red'
     DEBUG = False
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     ALLOWED_HOSTS = ['lifedjango.herokuapp.com']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
     import dj_database_url
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 else:
+    ENVIRONMENT_NAME = 'DEVELOPMENT'
+    ENVIRONMENT_COLOR = 'grey'
     DEBUG = True
     ALLOWED_HOSTS = []
     DATABASES = {
